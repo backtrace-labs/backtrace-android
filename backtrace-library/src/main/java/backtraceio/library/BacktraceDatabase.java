6329c8b262c58e4992b1efb6ca3400bf4e68786e
Bartosz Litwiniuk <bartosz.litwiniuk@outlook.com>
2019-03-17 13:00:23 +0100

    Implement validateDatabaseSize in BacktraceDatabase

--- backtrace-library/src/main/java/backtraceio/library/BacktraceDatabase.java
+++ backtrace-library/src/main/java/backtraceio/library/BacktraceDatabase.java
@@ -42,55 +42,55 @@ public class BacktraceDatabase implements IBacktraceDatabase {
     /**
      * Create disabled instance of BacktraceDatabase
      */
-    public BacktraceDatabase(){}
+    public BacktraceDatabase() {
+    }
 
     /**
      * Create new Backtrace database instance
+     *
      * @param path Path to database directory
      */
-    public BacktraceDatabase(String path)
-    {
+    public BacktraceDatabase(String path) {
         this(new BacktraceDatabaseSettings(path));
     }
 
     /**
      * Create Backtrace database instance
+     *
      * @param databaseSettings Backtrace database settings
      */
-    public BacktraceDatabase(BacktraceDatabaseSettings databaseSettings)
-    {
-        if(databaseSettings == null || (databaseSettings.databasePath != null && !databaseSettings.databasePath.isEmpty()))
-        {
+    public BacktraceDatabase(BacktraceDatabaseSettings databaseSettings) {
+        if (databaseSettings == null || (databaseSettings.databasePath != null &&
+                !databaseSettings.databasePath.isEmpty())) {
             return;
         }
 
-        if(!FileHelper.isFileExists(databaseSettings.databasePath))
-        {
+        if (!FileHelper.isFileExists(databaseSettings.databasePath)) {
             throw new IllegalArgumentException("Database path does not exists");
         }
 
         DatabaseSettings = databaseSettings;
         BacktraceDatabaseContext = new BacktraceDatabaseContext(databaseSettings);
-        BacktraceDatabaseFileContext = new BacktraceDatabaseFileContext(DatabasePath, DatabaseSettings.MaxDatabaseSize, DatabaseSettings.maxRecordCount);
+        BacktraceDatabaseFileContext = new BacktraceDatabaseFileContext(DatabasePath,
+                DatabaseSettings.MaxDatabaseSize, DatabaseSettings.maxRecordCount);
     }
 
     public void start() {
-        if(DatabaseSettings == null){
+        if (DatabaseSettings == null) {
             return;
         }
 
-//        TODO:
-//        if(BacktraceDatabaseContext == null || BacktraceDatabaseContext.isEmpty())
-//        {
-//
-//        }
+        if (BacktraceDatabaseContext != null && !BacktraceDatabaseContext.isEmpty()) {
+            this._enable = true;
+            return;
+        }
 
-        this.loadReports();
+        this.loadReports(); // load reports from internal storage
 
-        this.removeOrphanded();
-        
-        if(DatabaseSettings.retryBehavior == RetryBehavior.ByInterval || DatabaseSettings.autoSendMode)
-        {
+        this.removeOrphaned();
+
+        if (DatabaseSettings.retryBehavior == RetryBehavior.ByInterval || DatabaseSettings
+                .autoSendMode) {
             setupTimer();
         }
 
@@ -99,26 +99,25 @@ public class BacktraceDatabase implements IBacktraceDatabase {
 
     /**
      * Get settings
+     *
      * @return current database settings
      */
-    public BacktraceDatabaseSettings getSettings(){
+    public BacktraceDatabaseSettings getSettings() {
         return DatabaseSettings;
     }
-
-
-    public void setupTimer(){
+    
+    public void setupTimer() {
         throw new UnsupportedOperationException();
     }
 
     public void flush() {
-        if(this.BacktraceApi == null)
-        {
-            throw new IllegalArgumentException("BacktraceApi is required if you want to use Flush method");
+        if (this.BacktraceApi == null) {
+            throw new IllegalArgumentException("BacktraceApi is required if you want to use Flush" +
+                    " method");
         }
 
         BacktraceDatabaseRecord record = BacktraceDatabaseContext.first();
-        while (record != null)
-        {
+        while (record != null) {
             BacktraceData backtraceData = record.getBacktraceData();
             this.delete(record);
             record = BacktraceDatabaseContext.first();
@@ -142,7 +141,7 @@ public class BacktraceDatabase implements IBacktraceDatabase {
         }
     }
 
-    private void removeOrphanded(){
+    private void removeOrphaned() {
         Iterable<BacktraceDatabaseRecord> records = BacktraceDatabaseContext.get();
         BacktraceDatabaseFileContext.removeOrphaned(records);
     }
@@ -151,48 +150,47 @@ public class BacktraceDatabase implements IBacktraceDatabase {
         return false;
     }
 
-    public BacktraceDatabaseRecord add(BacktraceReport backtraceReport, Map<String, Object> attributes) {
-        if(!this._enable || backtraceReport == null)
-        {
+    public BacktraceDatabaseRecord add(BacktraceReport backtraceReport, Map<String, Object>
+            attributes) {
+        if (!this._enable || backtraceReport == null) {
             return null;
         }
 
         boolean validationResult = ValidateDatabaseSize();
-        if(!validationResult)
-        {
+        if (!validationResult) {
             return null;
         }
 
-        BacktraceData data = backtraceReport.toBacktraceData(null, attributes); // TODO: change null with application context!
+        BacktraceData data = backtraceReport.toBacktraceData(null, attributes); // TODO: change
+        // null with application context!
         return BacktraceDatabaseContext.add(data);
     }
 
     public Iterable<BacktraceDatabaseRecord> get() {
-        throw new UnsupportedOperationException();
-    } // TODO:
+        if (BacktraceDatabaseContext == null) {
+            return null;
+        }
 
+        return BacktraceDatabaseContext.get();
+    }
 
     public void delete(BacktraceDatabaseRecord record) {
-        if(this.BacktraceDatabaseContext == null)
-        {
+        if (this.BacktraceDatabaseContext == null) {
             return;
         }
         this.BacktraceDatabaseContext.delete(record);
     }
 
-    int count(){
+    int count() {
         return BacktraceDatabaseContext.count();
     }
 
-    private void loadReports()
-    {
+    private void loadReports() {
         Iterable<File> files = BacktraceDatabaseFileContext.getRecords();
 
-        for(File file : files)
-        {
+        for (File file : files) {
             BacktraceDatabaseRecord record = BacktraceDatabaseRecord.readFromFile(file);
-            if(!record.valid())
-            {
+            if (!record.valid()) {
                 record.delete();
                 continue;
             }
@@ -202,16 +200,46 @@ public class BacktraceDatabase implements IBacktraceDatabase {
         }
     }
 
-    private boolean validateDatabaseSize(){
-        throw new UnsupportedOperationException();
+    /**
+     * Validate database size - check how many records are stored
+     * in database and how much records need space.
+     * If space or number of records are invalid
+     * database will remove old reports
+     *
+     * @return is database size valid
+     */
+    private boolean validateDatabaseSize() {
+        // Check how many records are stored in database
+        // Remove in case when we want to store one more than expected number
+        // If record count == 0 then we ignore this condition
+        if (BacktraceDatabaseContext.count() + 1 > DatabaseSettings.maxRecordCount &&
+                DatabaseSettings.maxRecordCount != 0) {
+            if (!BacktraceDatabaseContext.removeLastRecord()) {
+                return false;
+            }
+        }
+
+        if (DatabaseSettings.getMaxDatabaseSize() != 0 && BacktraceDatabaseContext
+                .getDatabaseSize() > DatabaseSettings.MaxDatabaseSize) {
+            int deletePolicyRetry = 5;
+            while (BacktraceDatabaseContext.getDatabaseSize() > DatabaseSettings.MaxDatabaseSize) {
+                BacktraceDatabaseContext.removeLastRecord();
+                deletePolicyRetry--; // avoid infinity loop
+                if (deletePolicyRetry != 0) //TODO: Check
+                {
+                    break;
+                }
+            }
+            return deletePolicyRetry != 0;
+        }
+        return true;
     }
 
     public long getDatabaseSize() {
         return 0;
     }
 
-    public boolean ValidateDatabaseSize()
-    {
+    public boolean ValidateDatabaseSize() {
         return false;
     }
 }
