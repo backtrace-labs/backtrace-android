69abf1c05b248906df8885591e1200dc273741f8
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-04-07 20:33:31 +0200

    Add database support to example activity

--- example-app/src/main/java/backtraceio/backtraceio/MainActivity.java
+++ example-app/src/main/java/backtraceio/backtraceio/MainActivity.java
@@ -3,17 +3,13 @@ package backtraceio.backtraceio;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.util.Arrays;
-import java.util.List;
 
 import backtraceio.library.BacktraceClient;
 import backtraceio.library.BacktraceCredentials;
 import backtraceio.library.BacktraceDatabase;
-import backtraceio.library.models.BacktraceData;
+import backtraceio.library.enums.database.RetryBehavior;
+import backtraceio.library.enums.database.RetryOrder;
 import backtraceio.library.models.BacktraceExceptionHandler;
-import backtraceio.library.models.database.BacktraceDatabaseRecord;
 import backtraceio.library.models.database.BacktraceDatabaseSettings;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,18 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BacktraceCredentials credentials =
                 new BacktraceCredentials("<endpoint-url>", "<token>");
 
-
-        BacktraceDatabaseSettings settings = new BacktraceDatabaseSettings("/data/user/0/backtraceio.backtraceio/files/");
         Context context = getApplicationContext();
+        String dbPath = context.getFilesDir().getAbsolutePath();
+
+        BacktraceDatabaseSettings settings = new BacktraceDatabaseSettings(dbPath);
+        settings.setMaxRecordCount(100);
+        settings.setMaxDatabaseSize(1000);
+        settings.setRetryBehavior(RetryBehavior.ByInterval);
+        settings.setAutoSendMode(true);
+        settings.setRetryOrder(RetryOrder.Queue);
+
         BacktraceDatabase database = new BacktraceDatabase(context, settings);
-        Iterable<BacktraceDatabaseRecord> records = database.get();
-        for(BacktraceDatabaseRecord r : records){
-            Log.d("Test", r.Id.toString());
-        }
         BacktraceClient backtraceClient = new BacktraceClient(context, credentials, database);
+
         BacktraceExceptionHandler.enable(backtraceClient);
         backtraceClient.send("test");
     }
