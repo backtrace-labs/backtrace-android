f5fd62f32043dfd33e75acd50837ade7da2960b0
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-04-17 20:18:15 +0200

    Add logging to HttpAsyncTask, SourceCodeData, BacktraceDatabaseRecord and RecordWriter

--- backtrace-library/src/main/java/backtraceio/library/models/json/SourceCodeData.java
+++ backtrace-library/src/main/java/backtraceio/library/models/json/SourceCodeData.java
@@ -1,18 +1,18 @@
 package backtraceio.library.models.json;
 
-import com.google.gson.annotations.SerializedName;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import backtraceio.library.logger.BacktraceLogger;
 import backtraceio.library.models.BacktraceStackFrame;
 
 /**
  * Collect all source data information about current program
  */
 public class SourceCodeData {
+    private static transient final String LOG_TAG = SourceCodeData.class.getSimpleName();
+
     /**
      * Source code information about current executed program
      */
@@ -20,8 +20,10 @@ public class SourceCodeData {
 
     public SourceCodeData(ArrayList<BacktraceStackFrame> exceptionStack)
     {
+        BacktraceLogger.d(LOG_TAG, "Initialization source code data");
         if (exceptionStack == null || exceptionStack.size() == 0)
         {
+            BacktraceLogger.w(LOG_TAG, "Exception stack is null or empty");
             return;
         }
 
@@ -29,6 +31,7 @@ public class SourceCodeData {
         {
             if (backtraceStackFrame == null || backtraceStackFrame.sourceCode.equals(""))
             {
+                BacktraceLogger.w(LOG_TAG, "Stack frame is null or sourceCode is empty");
                 continue;
             }
             String id = backtraceStackFrame.sourceCode;
