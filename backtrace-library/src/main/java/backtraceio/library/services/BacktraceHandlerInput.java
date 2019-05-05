5fb76d500bc796db498451ce4c6bec15296f642f
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-05-05 21:22:58 +0200

    Add docs for BacktraceHandlerInput

--- backtrace-library/src/main/java/backtraceio/library/services/BacktraceHandlerInput.java
+++ backtrace-library/src/main/java/backtraceio/library/services/BacktraceHandlerInput.java
@@ -1,23 +1,44 @@
 package backtraceio.library.services;
 
-import java.util.List;
-
 import backtraceio.library.events.OnServerErrorEventListener;
 import backtraceio.library.events.OnServerResponseEventListener;
 import backtraceio.library.events.RequestHandler;
 import backtraceio.library.models.BacktraceData;
-import backtraceio.library.models.json.BacktraceReport;
 
+/**
+ * Class represents the message that will be sent to BacktraceHandlerThread
+ */
 public class BacktraceHandlerInput {
-    public BacktraceData data;
-    public OnServerResponseEventListener serverResponseEventListener;
-    public OnServerErrorEventListener serverErrorEventListener;
-    public RequestHandler requestHandler;
 
-    public BacktraceHandlerInput(BacktraceData data,
-                                 OnServerResponseEventListener serverResponseEventListener,
-                                 OnServerErrorEventListener serverErrorEventListener,
-                                 RequestHandler requestHandler) {
+    /**
+     * A message containing information about the error and the device
+     */
+    public BacktraceData data;
+    /**
+     * Event that will be started after receiving a response from the server
+     */
+    OnServerResponseEventListener serverResponseEventListener;
+    /**
+     * Event that will be started after receiving an error from the server
+     */
+    OnServerErrorEventListener serverErrorEventListener;
+    /**
+     * Event which will be executed instead of default sending report to Backtrace server
+     */
+    RequestHandler requestHandler;
+
+    /**
+     * Initialize new instance of BacktraceHandlerInput
+     *
+     * @param data                        a message containing information about the error and the device
+     * @param serverResponseEventListener event callback that will be started after receiving a response from the server
+     * @param serverErrorEventListener    event callback that will be started after receiving an error from the server
+     * @param requestHandler              event callback which will be executed instead of default sending report to Backtrace server
+     */
+    BacktraceHandlerInput(BacktraceData data,
+                          OnServerResponseEventListener serverResponseEventListener,
+                          OnServerErrorEventListener serverErrorEventListener,
+                          RequestHandler requestHandler) {
         this.data = data;
         this.serverResponseEventListener = serverResponseEventListener;
         this.serverErrorEventListener = serverErrorEventListener;
