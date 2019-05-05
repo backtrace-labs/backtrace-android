4fa070b4ff8beae93159bf1489d58fe656eec6c9
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-05-05 21:13:54 +0200

    Add docs for LogLevel class

--- backtrace-library/src/main/java/backtraceio/library/logger/LogLevel.java
+++ backtrace-library/src/main/java/backtraceio/library/logger/LogLevel.java
@@ -1,23 +1,22 @@
 package backtraceio.library.logger;
-// TODO:
 /**
- *
+ * The class is intended to determine the available levels of login messages
  */
 public enum LogLevel {
     /**
-     *
+     * logging level designed for logging debug messages
      */
     DEBUG,
     /**
-     *
+     * logging level designed for logging warning messages
      */
     WARN,
     /**
-     *
+     * logging level designed for logging errors messages
      */
     ERROR,
     /**
-     *
+     * No messages will be logged
      */
     OFF
 }
