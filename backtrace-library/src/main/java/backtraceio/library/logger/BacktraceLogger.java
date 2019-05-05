1bd51a4eeff562c2c01bee945e48732e47cf6a4f
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-05-05 21:14:07 +0200

    Add docs for BacktraceLogger class

--- backtrace-library/src/main/java/backtraceio/library/logger/BacktraceLogger.java
+++ backtrace-library/src/main/java/backtraceio/library/logger/BacktraceLogger.java
@@ -2,15 +2,31 @@ package backtraceio.library.logger;
 
 import android.util.Log;
 
+/**
+ * Backtrace Logger class for logging messages from inside library
+ */
 public class BacktraceLogger {
 
+    /**
+     * Level from which all information is logged
+     */
     private static LogLevel logLevel = LogLevel.OFF;
     private static final String BASE_TAG = "BacktraceLogger: ";
 
+    /**
+     * set logging level from which all messages should be logged to the console
+     * @param level login level
+     */
     public static void setLevel(LogLevel level) {
         BacktraceLogger.logLevel = level;
     }
 
+    /**
+     *
+     * @param tag source of logs, usually identifies the class or activity
+     * @param message text information which should be logged
+     * @return the number of bytes written
+     */
     public static int d(String tag, String message){
         if(BacktraceLogger.logLevel.ordinal() <= LogLevel.DEBUG.ordinal()) {
             return Log.d(getTag(tag), message);
@@ -18,6 +34,12 @@ public class BacktraceLogger {
         return 0;
     }
 
+    /**
+     * Log messages that suggest something unexpected or rare has happened, which isn't an error.
+     * @param tag source of logs, usually identifies the class or activity
+     * @param message text information which should be logged
+     * @return the number of bytes written
+     */
     public static int w(String tag, String message){
         if(BacktraceLogger.logLevel.ordinal() <= LogLevel.WARN.ordinal()) {
             return Log.w(getTag(tag), message);
@@ -25,6 +47,12 @@ public class BacktraceLogger {
         return 0;
     }
 
+    /**
+     * Log messages that suggest error or something that should not happen
+     * @param tag source of logs, usually identifies the class or activity
+     * @param message text information which should be logged
+     * @return the number of bytes written
+     */
     public static int e(String tag, String message){
         if(BacktraceLogger.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
             return Log.e(getTag(tag), message);
@@ -32,6 +60,13 @@ public class BacktraceLogger {
         return 0;
     }
 
+    /**
+     * Log messages that suggest error or something that should not happen
+     * @param tag source of logs, usually identifies the class or activity
+     * @param message text information which should be logged
+     * @param tr an exception to log
+     * @return the number of bytes written
+     */
     public static int e(String tag, String message, Throwable tr){
         if(BacktraceLogger.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
             return Log.e(getTag(tag), message, tr);
