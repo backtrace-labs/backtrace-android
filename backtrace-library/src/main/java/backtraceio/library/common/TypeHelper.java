6c09a6cfc714c5bfc8bd5ffab71e8ae56790cbcd
Konrad Dysput <konrad.dysput@gmail.com>
2021-01-29 13:52:51 +0100

    Extended primitive type comparision (#18)
    * Primitive types support
    
    * Removed double string type check and moved enum check to new type helper

---
+++ backtrace-library/src/main/java/backtraceio/library/common/TypeHelper.java
@@ -0,0 +1,18 @@
+package backtraceio.library.common;
+
+/**
+ * Backtrace TypeHelper helps with common type comparision.
+ */
+public class TypeHelper {
+    /**
+     * Check if object type is primitive - for example: int or long.
+     * @param type object to check
+     * @return true, if an object is primitive. Otherwise false.
+     */
+    public static boolean isPrimitiveOrPrimitiveWrapperOrString(Class type) {
+        return (type.isPrimitive() && type != void.class) ||
+                type == Double.class || type == Float.class || type == Long.class ||
+                type == Integer.class || type == Short.class || type == Character.class ||
+                type == Byte.class || type == Boolean.class || type == String.class || type.isEnum();
+    }
+}
