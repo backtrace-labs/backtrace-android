becd4dc079475d044384ba99771932a1916ea67b
Rameez Qurashi <rqurashi@backtrace.io>
2021-10-12 18:58:44 -0400

    Reduce number of stack frames of constructor chaining by supplying all defaults up front

--- backtrace-library/src/main/java/backtraceio/library/models/BacktraceMetricsSettings.java
+++ backtrace-library/src/main/java/backtraceio/library/models/BacktraceMetricsSettings.java
@@ -13,37 +13,36 @@ public class BacktraceMetricsSettings {
     /**
      * Backtrace metrics settings
      *
-     * @param universeName Backtrace universe name
-     * @param token        Backtrace submission token
+     * @param credentials Backtrace API credentials
      */
     public BacktraceMetricsSettings(BacktraceCredentials credentials) {
-        this(credentials, BacktraceMetrics.defaultBaseUrl);
+        this(credentials, BacktraceMetrics.defaultBaseUrl, BacktraceMetrics.defaultTimeIntervalMs, BacktraceMetrics.defaultTimeBetweenRetriesMs);
     }
 
     /**
      * Backtrace metrics settings
      *
-     * @param credentials   Backtrace API credentials
-     * @param baseUrl       Base URL to send metrics
+     * @param credentials Backtrace API credentials
+     * @param baseUrl     Base URL to send metrics
      */
     public BacktraceMetricsSettings(BacktraceCredentials credentials, String baseUrl) {
-        this(credentials, baseUrl, BacktraceMetrics.defaultTimeIntervalMs);
+        this(credentials, baseUrl, BacktraceMetrics.defaultTimeIntervalMs, BacktraceMetrics.defaultTimeBetweenRetriesMs);
     }
 
     /**
      * Backtrace metrics settings
      *
-     * @param credentials   Backtrace API credentials
+     * @param credentials        Backtrace API credentials
      * @param timeIntervalMillis Time interval between metrics auto-send events, 0 disables auto-send
      */
     public BacktraceMetricsSettings(BacktraceCredentials credentials, long timeIntervalMillis) {
-        this(credentials, BacktraceMetrics.defaultBaseUrl, timeIntervalMillis);
+        this(credentials, BacktraceMetrics.defaultBaseUrl, timeIntervalMillis, BacktraceMetrics.defaultTimeBetweenRetriesMs);
     }
 
     /**
      * Backtrace metrics settings
      *
-     * @param credentials   Backtrace API credentials
+     * @param credentials        Backtrace API credentials
      * @param baseUrl            Base URL to send metrics
      * @param timeIntervalMillis Time interval between metrics auto-send events, 0 disables auto-send
      */
@@ -54,7 +53,7 @@ public class BacktraceMetricsSettings {
     /**
      * Backtrace metrics settings
      *
-     * @param credentials   Backtrace API credentials
+     * @param credentials              Backtrace API credentials
      * @param baseUrl                  Base URL to send metrics
      * @param timeIntervalMillis       Time interval between metrics auto-send events, 0 disables auto-send
      * @param timeBetweenRetriesMillis Maximum time between retries in milliseconds
