f974438b53b8f364a007ae6b2ef4757618c58a0d
vlussenburg <vlussenburg@backtrace.io>
2022-08-03 17:11:43 -0600

    added espresso test + modified test script

--- example-app/src/androidTest/java/backtraceio/backtraceio/ExampleInstrumentedTest.java
+++ example-app/src/androidTest/java/backtraceio/backtraceio/ExampleInstrumentedTest.java
@@ -1,9 +1,16 @@
 package backtraceio.backtraceio;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.PerformException;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +23,28 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("backtraceio.backtraceio", appContext.getPackageName());
     }
+
+    @Test(expected = PerformException.class)
+    public void dumpWithoutCrash() {
+        onView(withId(R.id.dumpWithoutCrash)).perform(click());
+        onView(withId(R.id.handledException)).perform(click());
+        onView(withId(R.id.unhandledException)).perform(click());
+    }
+
+    // Will break build, obviously.
+//    @Test
+//    public void nativeCrash() {
+//        onView(withId(R.id.nativeCrash)).perform(click());
+//    }
 }
