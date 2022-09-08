43a8538839e76a200cee0a248aafe07df8a1c742
Bartosz Litwiniuk <>
2022-09-08 23:41:32 +0200

    Debug log

--- backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseRecordWriterTest.java
+++ backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseRecordWriterTest.java
@@ -72,6 +72,11 @@ public class BacktraceDatabaseRecordWriterTest {
         String jsonResult = FileHelper.readFile(new File(filePath));
         Exception exceptionResult = BacktraceSerializeHelper.fromJson(jsonResult, Exception.class);
 
+        // DEBUG
+        if(true) {
+            System.out.println(jsonResult);
+            throw new Exception(jsonResult);
+        }
         // THEN
         assertEquals(exception.getMessage(), exceptionResult.getMessage());
 
