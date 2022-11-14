a7cb17426587ecd016421d995e1dfec16731d4f0
Bartosz Litwiniuk <>
2022-11-15 00:03:35 +0100

    Remove coroner client

--- backtrace-library/src/main/java/backtraceio/library/coroner/query/CoronerQueryFields.java
+++
@@ -1,8 +0,0 @@
-package backtraceio.library.coroner.query;
-
-public class CoronerQueryFields {
-    public static String FOLD_CALLSTACK = "callstack";
-    public static String FOLD_CLASSIFIERS = "classifiers";
-    public static String FOLD_GUID = "guid";
-    public static String RXID = "_rxid";
-}
