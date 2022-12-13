b8348146c4717be9dcf71d3762d6b3db9e7886b2
Konrad Dysput <konrad.dysput@gmail.com>
2022-12-13 23:53:05 +0100

    Removed unnecessary import

--- backtrace-library/src/main/java/backtraceio/library/services/BacktraceMetrics.java
+++ backtrace-library/src/main/java/backtraceio/library/services/BacktraceMetrics.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
-import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedDeque;
