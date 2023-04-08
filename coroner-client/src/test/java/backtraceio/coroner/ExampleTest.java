88e6f0d096d5b7ec01ed68dcbdac5ce421db83ad
Bartosz Litwiniuk <>
2023-04-08 09:44:39 +0200

    Unit test config

---
+++ coroner-client/src/test/java/backtraceio/coroner/ExampleTest.java
@@ -0,0 +1,13 @@
+package backtraceio.coroner;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertEquals("a", "a");
+    }
+}
