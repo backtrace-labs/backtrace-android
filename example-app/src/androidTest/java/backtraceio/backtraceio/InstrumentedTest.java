a8a72b85350c6d38e5a93233184ca15834711c5a
Bartosz Litwiniuk <>
2023-04-24 23:35:55 +0200

    Fix

--- example-app/src/androidTest/java/backtraceio/backtraceio/InstrumentedTest.java
+++ example-app/src/androidTest/java/backtraceio/backtraceio/InstrumentedTest.java
@@ -11,8 +11,6 @@ public class InstrumentedTest {
     public static void init() {
         String url = BuildConfig.BACKTRACE_CORONER_URL;
         String coronerToken = BuildConfig.BACKTRACE_CORONER_TOKEN;
-//        String url = "https://yolo.sp.backtrace.io/api/query?project=android-library";
-//        String coronerToken = "4d1c52a829fa4b53c3c421d163f327e6bfaa5304e756a71249174ab2e1603f8a";
         coronerClient = new CoronerClient(url, coronerToken);
     }
 
