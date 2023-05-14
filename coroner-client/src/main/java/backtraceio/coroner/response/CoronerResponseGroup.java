e353728cce7650cbf6a8042c9af61f67a195628d
Bartosz Litwiniuk <>
2023-05-14 11:11:50 +0200

    Rename variable

--- coroner-client/src/main/java/backtraceio/coroner/response/CoronerResponseGroup.java
+++ coroner-client/src/main/java/backtraceio/coroner/response/CoronerResponseGroup.java
@@ -3,14 +3,14 @@ package backtraceio.coroner.response;
 import java.util.List;
 
 public class CoronerResponseGroup {
-    private static final Integer EXPECTED_NUMBER_OF_EL = 3;
+    private static final Integer EXPECTED_NUMBER_OF_ELEMENTS = 3;
     @SuppressWarnings("unused")
     private final String groupIdentifier;
     private final List<Object> values;
 
     public CoronerResponseGroup(final List<Object> obj) throws IllegalArgumentException {
-        if (obj == null || obj.size() != EXPECTED_NUMBER_OF_EL) {
-            throw new IllegalArgumentException("Wrong number of elements, expected number of elements: " + EXPECTED_NUMBER_OF_EL
+        if (obj == null || obj.size() != EXPECTED_NUMBER_OF_ELEMENTS) {
+            throw new IllegalArgumentException("Wrong number of elements, expected number of elements: " + EXPECTED_NUMBER_OF_ELEMENTS
                     + ", current value: " + (obj != null ? obj.size() : "null"));
         }
 
