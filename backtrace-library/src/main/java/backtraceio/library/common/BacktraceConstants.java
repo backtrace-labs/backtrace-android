e3f9e2ba0b0c33a1664b297d79a710dea7fbdef7
Ian Rice <irice@backtrace.io>
2023-06-01 11:04:07 -0400

    Added new constants for file extensions

---
+++ backtrace-library/src/main/java/backtraceio/library/common/BacktraceConstants.java
@@ -0,0 +1,6 @@
+package backtraceio.library.common;
+
+public final class BacktraceConstants {
+    public static final String MinidumpExtension = ".dmp";
+    public static final String MetadataExtension = ".meta";
+}
