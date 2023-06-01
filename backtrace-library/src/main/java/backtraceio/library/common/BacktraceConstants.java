43cd00dc4d86350037b4124ffbc3268e34d04020
Ian Rice <irice@backtrace.io>
2023-06-01 11:26:46 -0400

    Added Crashpad folder naming constants

--- backtrace-library/src/main/java/backtraceio/library/common/BacktraceConstants.java
+++ backtrace-library/src/main/java/backtraceio/library/common/BacktraceConstants.java
@@ -3,4 +3,7 @@ package backtraceio.library.common;
 public final class BacktraceConstants {
     public static final String MinidumpExtension = ".dmp";
     public static final String MetadataExtension = ".meta";
+    public static final String CompletedFolder = "completed";
+    public static final String NewFolder = "new";
+    public static final String PendingFolder = "pending";
 }
