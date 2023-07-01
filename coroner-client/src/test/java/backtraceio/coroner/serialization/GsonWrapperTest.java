24a807d1cb03b9860bb0a027b80a6ad8ce678325
Bartosz Litwiniuk <>
2023-07-01 09:45:37 +0200

    Move frames to new file

--- coroner-client/src/test/java/backtraceio/coroner/serialization/GsonWrapperTest.java
+++ coroner-client/src/test/java/backtraceio/coroner/serialization/GsonWrapperTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import static backtraceio.coroner.utils.ResourceUtils.EXPECTED_FRAMES;
 import static backtraceio.coroner.utils.ResourceUtils.RESPONSE_RX_FILTER_CORONER_JSON;
 import static backtraceio.coroner.utils.ResourceUtils.readResourceFile;
 
@@ -26,6 +27,7 @@ public class GsonWrapperTest {
     public void deserializeApiResponse() throws IOException{
         // GIVEN
         final String json = readResourceFile(RESPONSE_RX_FILTER_CORONER_JSON);
+        final String expectedFrames = readResourceFile(EXPECTED_FRAMES);
 
         // WHEN
         final CoronerApiResponse result = GsonWrapper.fromJson(json, CoronerApiResponse.class);
@@ -38,7 +40,7 @@ public class GsonWrapperTest {
 
         CoronerResponseGroup responseGroup = result.getResponse().values.get(0);
         assertEquals("Invalid index of selected element!", getResponseGroupAttributeValue(responseGroup.getAttribute(0)));
-        assertEquals("{\"frame\":[\"backtraceio.backtraceio.MainActivity.handledException\",\"androidx.appcompat.app.AppCompatViewInflater$DeclaredOnClickListener.onClick\",\"android.view.View.performClick\",\"android.view.View.performClickInternal\",\"android.view.View.access$3600\",\"android.view.View$PerformClick.run\",\"androidx.test.espresso.base.Interrogator.loopAndInterrogate\",\"androidx.test.espresso.base.UiControllerImpl.loopUntil\",\"androidx.test.espresso.base.UiControllerImpl.injectMotionEvent\",\"androidx.test.espresso.action.MotionEvents.sendUp\",\"androidx.test.espresso.action.Tap.sendSingleTap\",\"androidx.test.espresso.action.Tap.-$$Nest$smsendSingleTap\",\"androidx.test.espresso.action.Tap$1.sendTap\",\"androidx.test.espresso.action.GeneralClickAction.perform\",\"androidx.test.espresso.ViewInteraction$SingleExecutionViewAction.perform\",\"androidx.test.espresso.ViewInteraction.doPerform\",\"androidx.test.espresso.ViewInteraction.-$$Nest$mdoPerform\",\"androidx.test.espresso.ViewInteraction$1.call\",\"java.util.concurrent.FutureTask.run\",\"android.os.Handler.handleCallback\",\"android.os.Handler.dispatchMessage\",\"android.os.Looper.loop\",\"android.app.ActivityThread.main\",\"java.lang.reflect.Method.invoke\",\"com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run\",\"com.android.internal.os.ZygoteInit.main\"]}", getResponseGroupAttributeValue(responseGroup.getAttribute(1)));
+        assertEquals(expectedFrames, getResponseGroupAttributeValue(responseGroup.getAttribute(1)));
         assertEquals("e4c57699-0dc9-35e2-b4a0-2ffff1925ca7", getResponseGroupAttributeValue(responseGroup.getAttribute(2)));
         assertEquals("java.lang.IndexOutOfBoundsException", getResponseGroupAttributeValue(responseGroup.getAttribute(3)));
     }
--- coroner-client/src/test/java/backtraceio/coroner/utils/ResourceUtils.java
+++ coroner-client/src/test/java/backtraceio/coroner/utils/ResourceUtils.java
@@ -10,6 +10,7 @@ public class ResourceUtils {
     public static final String QUERY_CORONER_RXID_123_ATTR_ERR_MSG = MAIN_PATH + "/queries/rxid-12345-custom-attr-err-msg.json";
     public static final String QUERY_CORONER_TIMESTAMP_ERR_TYPE= MAIN_PATH + "/queries/timestamp-err-type-filter.json";
     public static final String RESPONSE_RX_FILTER_CORONER_JSON = MAIN_PATH + "/responses/rx-filter-response.json";
+    public static final String EXPECTED_FRAMES = MAIN_PATH + "/frames.json";
     public static final String RESPONSE_TIMESTAMP_ERR_TYPE_CORONER_JSON = MAIN_PATH + "/responses/timestamp-err-type-filter.json";
 
     public static final String RESPONSE_OPERATION_ERROR_JSON = MAIN_PATH + "/responses/operation-error.json";
