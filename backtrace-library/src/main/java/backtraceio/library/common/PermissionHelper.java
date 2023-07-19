83eeb46e5e7365268466eee9bec2074103af893c
harissauce <melek.ramki@saucelabs.com>
2023-07-19 15:54:39 -0400

    Update PermissionHelper.java
    Replace ContextCompat with Context when checking for self permissions
    Return granted when checking for self permissions in Android versions prior to Android 6.0

--- backtrace-library/src/main/java/backtraceio/library/common/PermissionHelper.java
+++ backtrace-library/src/main/java/backtraceio/library/common/PermissionHelper.java
@@ -3,7 +3,7 @@ package backtraceio.library.common;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import androidx.core.content.ContextCompat;
+import android.os.Build;
 
 /***
  * Helper class for checking permissions
@@ -11,38 +11,58 @@ import androidx.core.content.ContextCompat;
 public class PermissionHelper {
 
     /**
-     * Check is permission for Bluetooth is granted (permission.BLUETOOTH)
+     * Check if permission for Bluetooth is granted (permission.BLUETOOTH)
      *
      * @return true if permission is granted
+     * For Build versions prior to Android 6.0, permissions are granted during installation
      */
     public static boolean isPermissionForBluetoothGranted(Context context) {
-        return ContextCompat.checkSelfPermission(context,
-                Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return context.checkSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
     }
 
-
     /**
-     * Check is permission for Bluetooth is granted (permission.INTERNET)
+     * Check if permission for Internet is granted (permission.INTERNET)
      *
      * @return true if permission is granted
+     * For Build versions prior to Android 6.0, permissions are granted during installation
      */
     public static boolean isPermissionForInternetGranted(Context context) {
-        return ContextCompat.checkSelfPermission(context,
-                Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    public static boolean isPermissionForAccessWifiStateGranted(Context context) {
-        return ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return context.checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
     }
 
     /**
-     * Check is permission for Read external storage grande
+     * Check if permission for Wifi state is granted (permission.ACCESS_WIFI_STATE)
      *
      * @return true if permission is granted
+     * For Build versions prior to Android 6.0, permissions are granted during installation
+     */
+    public static boolean isPermissionForAccessWifiStateGranted(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return context.checkSelfPermission(Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Check if permission to Read external storage is granted (permission.READ_EXTERNAL_STORAGE)
+     *
+     * @return true if permission is granted
+     * For Build versions prior to Android 6.0, permissions are granted during installation
      */
     public static boolean isPermissionForReadExternalStorageGranted(Context context) {
-        return ContextCompat.checkSelfPermission(context,
-                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
     }
 }
