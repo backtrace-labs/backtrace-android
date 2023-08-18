86c0a3b31999cf1a6eed8ab505ef79f9b1686aa5
Ian Rice <irice@backtrace.io>
2023-08-18 13:48:01 -0400

    Made variables final where possible

--- backtrace-library/src/main/java/backtraceio/library/services/BacktraceNativeCrashResponseListener.java
+++ backtrace-library/src/main/java/backtraceio/library/services/BacktraceNativeCrashResponseListener.java
@@ -1,14 +1,10 @@
 package backtraceio.library.services;
 
-import android.util.Log;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
-import backtraceio.library.BacktraceClient;
 import backtraceio.library.base.BacktraceBase;
 import backtraceio.library.common.BacktraceConstants;
 import backtraceio.library.events.OnServerResponseEventListener;
@@ -16,24 +12,25 @@ import backtraceio.library.logger.BacktraceLogger;
 import backtraceio.library.models.BacktraceResult;
 import backtraceio.library.models.json.BacktraceReport;
 import backtraceio.library.models.types.BacktraceResultStatus;
-import kotlin.text.Regex;
 
 public class BacktraceNativeCrashResponseListener
         implements OnServerResponseEventListener {
     private static final String LOG_TAG = BacktraceNativeCrashResponseListener.
             class.getSimpleName();
 
-    private static BacktraceNativeCrashResponseListener instance;
+    private final static BacktraceNativeCrashResponseListener instance = new BacktraceNativeCrashResponseListener();
+    private final static FilenameFilter minidumpFilter = (dir, name) -> name.endsWith(BacktraceConstants.MinidumpExtension);
 
-    private BacktraceNativeCrashResponseListener() {}
+    private BacktraceNativeCrashResponseListener() {
+    }
 
     @Override
     public void onEvent(BacktraceResult backtraceResult) {
         if (backtraceResult.status == BacktraceResultStatus.Ok) {
             // Get the minidump out
-            BacktraceReport report = backtraceResult.getBacktraceReport();
+            final BacktraceReport report = backtraceResult.getBacktraceReport();
             if (report != null) {
-                List<String> attachments = report.attachmentPaths;
+                final List<String> attachments = report.attachmentPaths;
                 for (String attachment : attachments) {
                     if (attachment.endsWith(BacktraceConstants.MinidumpExtension)) {
                         moveToCompleted(attachment);
@@ -53,14 +50,11 @@ public class BacktraceNativeCrashResponseListener
             BacktraceLogger.e(LOG_TAG, "Database path cannot be null");
             return;
         }
-        if (instance == null) {
-            instance = new BacktraceNativeCrashResponseListener();
-        }
-        Thread sender = new Thread(() -> {
-            List<File> minidumps = scanForNewMinidumps(databasePath);
+        final Thread sender = new Thread(() -> {
+            final List<File> minidumps = scanForNewMinidumps(databasePath);
             if (minidumps.size() > 0) {
-                for (File minidump: minidumps) {
-                    BacktraceReport report = createBacktraceReport(minidump, databasePath);
+                for (File minidump : minidumps) {
+                    final BacktraceReport report = createBacktraceReport(minidump, databasePath);
                     client.send(report, instance);
                 }
             }
@@ -69,31 +63,31 @@ public class BacktraceNativeCrashResponseListener
     }
 
     private static List<File> scanForNewMinidumps(String databasePath) {
-        List<File> minidumps = new ArrayList<>();
-        FilenameFilter minidumpFilter = (dir, name) -> name.endsWith(BacktraceConstants.MinidumpExtension);
-        String[] newFiles = new File(databasePath + "/" + BacktraceConstants.NewFolder).list(minidumpFilter);
-        String[] pendFiles = new File(databasePath + "/" + BacktraceConstants.PendingFolder).list(minidumpFilter);
-        if (newFiles != null) {
-            for (String file : newFiles) {
-                minidumps.add(new File(databasePath + "/" + BacktraceConstants.NewFolder + "/" + file));
-            }
-        }
-        if (pendFiles != null) {
-            for (String file : pendFiles) {
-                minidumps.add(new File(databasePath + "/" + BacktraceConstants.PendingFolder + "/" + file));
-            }
-        }
+        final List<File> minidumps = new ArrayList<>();
+        minidumps.addAll(getFiles(databasePath + "/" + BacktraceConstants.NewFolder));
+        minidumps.addAll(getFiles(databasePath + "/" + BacktraceConstants.PendingFolder));
         return minidumps;
     }
 
+    private static List<File> getFiles(String dirPath) {
+        final String[] dirFiles = new File(dirPath).list(minidumpFilter);
+        final List<File> result = new ArrayList<>();
+        if (dirFiles != null) {
+            for (String file : dirFiles) {
+                result.add(new File(dirPath + "/" + file));
+            }
+        }
+        return result;
+    }
+
     private static BacktraceReport createBacktraceReport(File minidump, String databasePath) {
-        BacktraceReport report = new BacktraceReport("");
+        final BacktraceReport report = new BacktraceReport("");
         report.attachmentPaths.add(minidump.getAbsolutePath());
-        String attachmentPath = databasePath + "/" + BacktraceConstants.AttachmentsFolder + "/"
+        final String attachmentPath = databasePath + "/" + BacktraceConstants.AttachmentsFolder + "/"
                 + minidump.getName().replace(BacktraceConstants.MinidumpExtension, "");
-        File attachmentsDir = new File(attachmentPath);
+        final File attachmentsDir = new File(attachmentPath);
         if (attachmentsDir.isDirectory()) {
-            for (String file: attachmentsDir.list()) {
+            for (String file : attachmentsDir.list()) {
                 report.attachmentPaths.add(attachmentsDir.getAbsolutePath() + "/" + file);
             }
         }
@@ -101,21 +95,21 @@ public class BacktraceNativeCrashResponseListener
     }
 
     private void moveToCompleted(String minidumpPath) {
-        String regex = "/(" + BacktraceConstants.NewFolder + "|" + BacktraceConstants.PendingFolder
+        final String regex = "/(" + BacktraceConstants.NewFolder + "|" + BacktraceConstants.PendingFolder
                 + ")/";
-        String minidumpOutputPath = minidumpPath.replaceAll(regex, "/" + BacktraceConstants.CompletedFolder + "/");
-        String metaDataPath = minidumpPath.replace(BacktraceConstants.MinidumpExtension,
+        final String minidumpOutputPath = minidumpPath.replaceAll(regex, "/" + BacktraceConstants.CompletedFolder + "/");
+        final String metaDataPath = minidumpPath.replace(BacktraceConstants.MinidumpExtension,
                 BacktraceConstants.MetadataExtension);
-        String metaDataCompletedPath = metaDataPath.replaceAll(regex, "/" + BacktraceConstants.CompletedFolder + "/");
+        final String metaDataCompletedPath = metaDataPath.replaceAll(regex, "/" + BacktraceConstants.CompletedFolder + "/");
         if (moveFile(minidumpPath, minidumpOutputPath))
             moveFile(metaDataPath, metaDataCompletedPath);
     }
 
     private boolean moveFile(String fromPath, String toPath) {
-        File from = new File(fromPath);
+        final File from = new File(fromPath);
         if (from.exists()) {
-            File to = new File(toPath);
-            boolean success = from.renameTo(to);
+            final File to = new File(toPath);
+            final boolean success = from.renameTo(to);
             if (success) {
                 from.delete();
                 return true;
