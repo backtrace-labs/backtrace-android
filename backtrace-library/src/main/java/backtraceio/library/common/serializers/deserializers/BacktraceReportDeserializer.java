3686709bdd9676f7546a6baa0f2cbcc6341c3432
Bartosz Litwiniuk <>
2024-02-06 17:58:36 +0100

    Serialize exception

--- backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/BacktraceReportDeserializer.java
+++ backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/BacktraceReportDeserializer.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -17,20 +18,23 @@ import backtraceio.library.models.json.BacktraceReport;
 
 public class BacktraceReportDeserializer implements Deserializable<BacktraceReport> {
 
-
+    ExceptionDeserializer exceptionDeserializer;
+    public BacktraceReportDeserializer() {
+        this.exceptionDeserializer = new ExceptionDeserializer();
+    }
     @Override
     // TODO: fix all null warnings
     public BacktraceReport deserialize(JSONObject obj) throws JSONException {
         final String uuid = obj.optString("uuid", null);
         final long timestamp = obj.optLong("timestamp", 0);
-        final String message = obj.optString("message", null); // TODO fix
-        final String classifier = obj.optString("classifier", null);
+        final String message = obj.optString("message", null);
+        final String classifier = obj.optString("classifier", "");
         final boolean exceptionTypeReport = obj.optBoolean("exception-type-report");
-        final Exception exception = getException(obj.optJSONObject("exception")); // TODO: fix
-        final Map<String, Object> attributes = new HashMap<>(); // TODO: fix
+        final Exception exception = getException(obj.optJSONObject("exception"));
+        final Map<String, Object> attributes = this.getAttributes(obj.optJSONObject("attributes")); // TODO: fix
 
         final List<String> attachmentPaths = this.getAttachmentList(obj.optJSONArray("attachmentPaths"));
-        final List<BacktraceStackFrame> diagnosticStack = this.getBacktraceStackFrame();
+        final List<BacktraceStackFrame> diagnosticStack = this.getDiagnosticStack(obj.optJSONArray("diagnostic-stack")); // todo fix diagnostic-stack name
 
         return new BacktraceReport(
                 !isNullOrEmpty(uuid)? UUID.fromString(uuid) : null,
@@ -46,16 +50,28 @@ public class BacktraceReportDeserializer implements Deserializable<BacktraceRepo
     }
 
     public Exception getException(JSONObject obj) {
-        return null; // TODO:
+        return this.exceptionDeserializer.deserialize(obj); // TODO: fix usage
     }
 
     public Map<String, Object> getAttributes(JSONObject obj) {
-        return null; // TODO:
+        if (obj == null) {
+            return null;
+        }
+
+        Map<String, Object> result = new HashMap<>();
+
+        Iterator<String> attributesKeys = obj.keys();
+        while (attributesKeys.hasNext()) {
+            String key = attributesKeys.next();
+            result.put(key, obj.optString(key));
+        }
+
+        return result;
     }
 
     public List<String> getAttachmentList(JSONArray array) {
         if (array == null) {
-            return null;
+            return new ArrayList<>();
         }
 
         List<String> result = new ArrayList<>();
@@ -67,7 +83,23 @@ public class BacktraceReportDeserializer implements Deserializable<BacktraceRepo
         return result;
     }
 
-    public List<BacktraceStackFrame> getBacktraceStackFrame() {
-        return null; // TODO
+    public List<BacktraceStackFrame> getDiagnosticStack(JSONArray obj) {
+        if (obj == null) {
+            return null;
+        }
+
+        List<BacktraceStackFrame> result = new ArrayList<>();
+        for (int i = 0; i < obj.length(); i++) {
+            JSONObject stackItem = obj.optJSONObject(i);
+            if (stackItem != null) {
+                BacktraceStackFrame stackFrame = new BacktraceStackFrame();
+                stackFrame.functionName = stackItem.optString("function-name");
+                stackFrame.line = stackItem.optInt("line"); // todo: should be null in case of empty
+                stackFrame.sourceCode = stackItem.optString("source-code");
+                result.add(stackFrame);
+            }
+        }
+
+        return result;
     }
 }
--- backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/ExceptionDeserializer.java
+++ backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/ExceptionDeserializer.java
@@ -1,11 +1,47 @@
 package backtraceio.library.common.serializers.deserializers;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExceptionDeserializer implements Deserializable<Exception> {
     @Override
-    public Exception deserialize(JSONObject obj) throws JSONException {
-        return null; // TODO: fix
+    public Exception deserialize(JSONObject obj) {
+        final String message = obj.optString("detail-message", "");
+
+        final Exception exception = new Exception(message);
+
+        try {
+            exception.setStackTrace(getStacktrace(obj.optJSONArray("stack-trace")));
+        }
+        catch (JSONException jsonException) {
+            //TODO: handle
+            System.out.println(jsonException.toString());
+        }
+        return exception;
+    }
+
+    public StackTraceElement[] getStacktrace(JSONArray array) throws JSONException {
+        if (array == null || array.length() == 0) {
+            // TODO:
+            return null;
+        }
+
+        StackTraceElement[] result = new StackTraceElement[array.length()];
+
+        for(int idx = 0; idx < array.length(); idx++) {
+            JSONObject obj = (JSONObject) array.get(idx);
+            result[idx] = new StackTraceElement(
+                obj.optString("declaring-class"), // make something to not hardcode in this way
+                    obj.getString("method-name"), // make something to not hardcode in this way
+                    obj.optString("file-name"), // make something to not hardcode in this way
+                    obj.getInt("line-number") // make something to not hardcode in this way
+            );
+        }
+
+        return result;
     }
 }
