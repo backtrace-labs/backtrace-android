3f21f87bd3822a508a30d1bc7691965a03f551d7
Bartosz Litwiniuk <>
2024-02-11 11:34:08 +0100

    Implementation of next deserialization methods of Backtrace Data obj

--- backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/BacktraceDataDeserializer.java
+++ backtrace-library/src/main/java/backtraceio/library/common/serializers/deserializers/BacktraceDataDeserializer.java
@@ -1,16 +1,22 @@
 package backtraceio.library.common.serializers.deserializers;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import backtraceio.library.models.BacktraceData;
-import backtraceio.library.models.BacktraceResult;
-import backtraceio.library.models.types.BacktraceResultStatus;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import backtraceio.library.models.BacktraceData;
+import backtraceio.library.models.json.BacktraceReport;
+import backtraceio.library.models.json.SourceCode;
+import backtraceio.library.models.json.ThreadInformation;
+
+// TODO: check if methods should be private
 public class BacktraceDataDeserializer implements Deserializable<BacktraceData>{
 
     public BacktraceData deserialize(JSONObject obj) throws JSONException {
-
         return new BacktraceData(
               obj.optString("uuid"),
                 obj.optString("symbolication"),
@@ -18,14 +24,77 @@ public class BacktraceDataDeserializer implements Deserializable<BacktraceData>{
                 obj.optString("lang-version"), // TODO: fix or improve casing should get from annotation
                 obj.optString("agent-version"), // TODO: fix or improve casing should get from annotation
                 // TODO: fix all below
-                null,
+                getAttributes(obj.optJSONObject("attributes")),
                 obj.optString("main-thread"),
                 // TODO: deserialize all of below
-                null,
-                null,
-                null,
-                null,
-                null
+                getClassifiers(obj.optJSONArray("classifiers")),
+                getBacktraceReport(obj.optJSONObject("report")),
+                getAnnotations(obj.optJSONObject("annotations")),
+                getSourceCode(obj.optJSONObject("sourceCode")),
+
+                getThreadInformation(obj.optJSONObject("threads"))
         ); // TODO
+    }
+
+    public BacktraceReport getBacktraceReport(JSONObject obj) {
+        // TODO: implement
+        return null;
+    }
+
+    public Map<String, Object> getAnnotations(JSONObject obj) {
+        // TODO: implement
+        return null;
+    }
+
+    public Map<String, SourceCode> getSourceCode(JSONObject obj) {
+        // TODO: implement
+        return null;
+    }
+
+    public Map<String, ThreadInformation> getThreadInformation(JSONObject obj) {
+        // TODO: implement
+        return null;
+    }
+
+    public String [] getClassifiers(JSONArray jsonArray) {
+
+        if (jsonArray == null) {
+            return new String[0];
+        }
+
+        String[] result = new String[jsonArray.length()];
+
+        for(int i = 0; i < jsonArray.length(); i++){
+            try {
+                Object object = jsonArray.get(i);
+                result[i] = object.toString();
+            }
+            catch (JSONException exception) {
+                // todo: error handling
+            }
+        }
+
+        return result;
+    }
+
+    public Map<String, String> getAttributes(JSONObject obj) {
+        Map<String, String> result = new HashMap<>();
+
+        if (obj == null) {
+            return result;
+        }
+
+        Iterator<String> keys = obj.keys();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            try {
+                result.put(key, obj.get(key).toString());
+            } catch (JSONException e) {
+                // TODO: error handling
+            }
+        }
+
+        return result;
     }
 }
--- backtrace-library/src/test/java/backtraceio/library/BacktraceApiResultDeserializerTest.java
+++ backtrace-library/src/test/java/backtraceio/library/BacktraceApiResultDeserializerTest.java
@@ -10,11 +10,11 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import backtraceio.library.common.serializers.deserializers.BacktraceApiResultDeserializer;
-import backtraceio.library.models.BacktraceResult;
+import backtraceio.library.models.BacktraceApiResult;
 import backtraceio.library.models.types.BacktraceResultStatus;
 
 // TODO: move to standard unit tests not instrumented
-public class BacktraceResultDeserializerTest {
+public class BacktraceApiResultDeserializerTest {
     @Test
     public void deserializeCoronerApiJsonResponse() throws JSONException {
         // GIVEN
@@ -22,13 +22,13 @@ public class BacktraceResultDeserializerTest {
 
         // WHEN
         BacktraceApiResultDeserializer deserializer = new BacktraceApiResultDeserializer();
-        BacktraceResult result = deserializer.deserialize(new JSONObject(json));
+        BacktraceApiResult result = deserializer.deserialize(new JSONObject(json));
 
         // THEN
         assertNotNull(result);
-        assertNull(result.getBacktraceReport());
-        assertNull(result.message);
-        assertEquals(BacktraceResultStatus.Ok, result.status);
+//        assertNull(result.getBacktraceReport());
+//        assertNull(result.message);
+        assertEquals(BacktraceResultStatus.Ok, result.response);
         assertEquals("01000000-5360-240b-0000-000000000000", result.rxId);
     }
 
