f96cc52cc128c496b4c6e2fa488275dcb9065c39
Bartosz Litwiniuk <>
2024-10-23 08:15:47 +0200

    Add BacktraceResultStatus tests

--- backtrace-library/src/test/java/backtraceio/library/models/types/BacktraceResultStatusTest.java
+++ backtrace-library/src/test/java/backtraceio/library/models/types/BacktraceResultStatusTest.java
@@ -3,42 +3,51 @@ package backtraceio.library.models.types;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class BacktraceResultStatusTest {
-    private String apiStatus;
-    private BacktraceResultStatus expectedApiStatusEnum;
+    @RunWith(Parameterized.class)
+    public static class BacktraceResultStatusParametrizedTests {
+        private final String apiStatus;
+        private final BacktraceResultStatus expectedApiStatusEnum;
 
-    public BacktraceResultStatusTest(String apiStatus, BacktraceResultStatus expectedApiStatusEnum) {
-        this.apiStatus = apiStatus;
-        this.expectedApiStatusEnum = expectedApiStatusEnum;
-    }
-    @Parameterized.Parameters
-    public static Collection apiStringStatus() {
-        return Arrays.asList(new Object[][] {
-                { "ok", BacktraceResultStatus.Ok},
-                { "Ok", BacktraceResultStatus.Ok },
-                { "OK", BacktraceResultStatus.Ok },
-                { "oK", BacktraceResultStatus.Ok },
-                { "servererror", BacktraceResultStatus.ServerError },
-                { "serverError", BacktraceResultStatus.ServerError },
-                { "ServerError", BacktraceResultStatus.ServerError },
-                { "SERVERERROR", BacktraceResultStatus.ServerError }
-        });
-    }
-    @Test
-    public void testMappingStatusStringToEnum() {
-        assertEquals(this.expectedApiStatusEnum,
-                BacktraceResultStatus.enumOf(this.apiStatus));
-    }
+        public BacktraceResultStatusParametrizedTests(String apiStatus, BacktraceResultStatus expectedApiStatusEnum) {
+            this.apiStatus = apiStatus;
+            this.expectedApiStatusEnum = expectedApiStatusEnum;
+        }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testWrongStatusValue() {
-        BacktraceResultStatus.enumOf("unsupported-value");
+        @Parameterized.Parameters
+        public static Collection apiStringStatus() {
+            return Arrays.asList(new Object[][]{
+                    {"ok", BacktraceResultStatus.Ok},
+                    {"Ok", BacktraceResultStatus.Ok},
+                    {"OK", BacktraceResultStatus.Ok},
+                    {"oK", BacktraceResultStatus.Ok},
+                    {"servererror", BacktraceResultStatus.ServerError},
+                    {"serverError", BacktraceResultStatus.ServerError},
+                    {"ServerError", BacktraceResultStatus.ServerError},
+                    {"SERVERERROR", BacktraceResultStatus.ServerError}
+            });
+        }
+
+        @Test
+        public void testMappingStatusStringToEnum() {
+            assertEquals(this.expectedApiStatusEnum,
+                    BacktraceResultStatus.enumOf(this.apiStatus));
+        }
+
+    }
+    public static class BacktraceResultStatusSingleTests {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void testWrongStatusValue() {
+            BacktraceResultStatus.enumOf("unsupported-value");
+        }
     }
 }
