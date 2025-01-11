3c081832da173550fd71231fc0cf66a4aa0d0eac
Bartosz Litwiniuk <>
2025-01-11 08:41:57 +0100

    Add BacktraceDataSerializer test

--- backtrace-library/src/androidTest/java/backtraceio/library/models/BacktraceDataTest.java
+++ backtrace-library/src/androidTest/java/backtraceio/library/models/BacktraceDataTest.java
@@ -2,6 +2,8 @@ package backtraceio.library.models;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
@@ -9,6 +11,8 @@ import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import backtraceio.library.common.json.naming.NamingPolicy;
+import backtraceio.library.common.json.serialization.BacktraceDataSerializer;
 import backtraceio.library.models.json.BacktraceReport;
 
 @RunWith(AndroidJUnit4.class)
@@ -30,7 +36,7 @@ public class BacktraceDataTest {
     }
 
     @Test
-    public void createBacktraceDataTest() {
+    public void createBacktraceDataBuilderTest() {
         // GIVEN
         List<String> attachmentsPath = Arrays.asList("one", "two", "three");
         BacktraceReport report = new BacktraceReport(new IllegalAccessException("test-message"), attachmentsPath);
@@ -63,19 +69,22 @@ public class BacktraceDataTest {
         assertEquals("true", backtraceData.getAttributes().get("attr-2"));
         assertEquals("test", backtraceData.getAttributes().get("attr-3"));
     }
-    @Test
-    public void testBacktraceDataConstructor() {
-        // GIVEN
-        List<String> attachmentsPath = Arrays.asList("one", "two", "three");
-        BacktraceReport report = new BacktraceReport(new IllegalAccessException("test-message"), attachmentsPath);
 
+    public BacktraceData createBacktraceDataObj(BacktraceReport report) {
         Map<String, Object> clientAttributes = new HashMap<>();
         clientAttributes.put("attr-1", 1);
         clientAttributes.put("attr-2", true);
         clientAttributes.put("attr-3", "test");
 
         // WHEN
-        BacktraceData backtraceData = new BacktraceData(context, report, clientAttributes);
+        return new BacktraceData(context, report, clientAttributes);
+    }
+    @Test
+    public void testBacktraceDataConstructor() {
+        // GIVEN
+        List<String> attachmentsPath = Arrays.asList("one", "two", "three");
+        BacktraceReport report = new BacktraceReport(new IllegalAccessException("test-message"), attachmentsPath);
+        BacktraceData backtraceData = createBacktraceDataObj(report);
 
         // THEN
         assertArrayEquals( new String[]{"java.lang.IllegalAccessException"}, backtraceData.getClassifiers());
@@ -92,10 +101,36 @@ public class BacktraceDataTest {
         assertEquals(3, backtraceData.getAnnotations().size());
         assertEquals("instr: androidx.test.runner.androidjunitrunner", backtraceData.getMainThread());
         assertEquals(34, backtraceData.getSourceCode().size());
-        assertTrue(!backtraceData.getThreadInformationMap().isEmpty());
+        assertFalse(backtraceData.getThreadInformationMap().isEmpty());
         assertEquals(3, backtraceData.getAttachmentPaths().size());
         assertEquals("1", backtraceData.getAttributes().get("attr-1"));
         assertEquals("true", backtraceData.getAttributes().get("attr-2"));
         assertEquals("test", backtraceData.getAttributes().get("attr-3"));
+    }
+    
+    @Test
+    public void serializeBacktraceData() throws JSONException {
+        // GIVEN
+        List<String> attachmentsPath = Arrays.asList("one", "two", "three");
+        BacktraceReport report = new BacktraceReport(new IllegalAccessException("test-message"), attachmentsPath);
+        BacktraceData backtraceData = createBacktraceDataObj(report);
+
+        // WHEN
+        JSONObject json = new BacktraceDataSerializer(new NamingPolicy()).toJson(backtraceData);
+
+        // THEN
+        assertNotNull(json);
+        assertEquals("java", json.getString("lang"));
+        assertEquals("backtrace-android", json.getString("agent"));
+        assertEquals("", json.getString("symbolication"));
+        assertEquals(36, json.getString("uuid").length());
+        assertEquals("instr: androidx.test.runner.androidjunitrunner", json.getString("mainThread"));
+        assertEquals(1, json.getJSONArray("classifiers").length());
+        assertEquals("java.lang.IllegalAccessException", json.getJSONArray("classifiers").get(0));
+        assertTrue(json.getLong("timestamp") > 0);
+        assertNotNull(json.getJSONObject("attributes"));
+        assertNotNull(json.getJSONObject("annotations"));
+        assertNotNull(json.getJSONObject("sourceCode"));
+        assertNotNull(json.getJSONObject("threads"));
     }
 }
