d72974e5d15de2a7302732aa169a9960c3ea1eb5
Bartosz Litwiniuk <>
2025-05-06 22:08:12 +0200

    Fix values

--- backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseContextMultithreadedTest.java
+++ backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseContextMultithreadedTest.java
@@ -61,7 +61,7 @@ public class BacktraceDatabaseContextMultithreadedTest {
     @Test
     public void testConcurrentModification() throws InterruptedException {
         // GIVEN
-        final TestConfig config = new TestConfig(1000, 500, 750, 500000);
+        final TestConfig config = new TestConfig(100, 50, 75, 2000);
         final List<BacktraceDatabaseRecord> initialRecords = generateMockRecords(config.recordsState);
         
         final CountDownLatch startLatch = new CountDownLatch(1);
