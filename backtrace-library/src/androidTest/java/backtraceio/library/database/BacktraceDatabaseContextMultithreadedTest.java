a13435bc08224b36bcc8365195f248d17a886778
Bartosz Litwiniuk <>
2025-05-06 23:24:31 +0200

    Increase timeout

--- backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseContextMultithreadedTest.java
+++ backtrace-library/src/androidTest/java/backtraceio/library/database/BacktraceDatabaseContextMultithreadedTest.java
@@ -61,7 +61,7 @@ public class BacktraceDatabaseContextMultithreadedTest {
     @Test
     public void testConcurrentModification() throws InterruptedException {
         // GIVEN
-        final TestConfig config = new TestConfig(500, 250, 150, 10000);
+        final TestConfig config = new TestConfig(500, 250, 150, 3000); // 30s
         final List<BacktraceDatabaseRecord> initialRecords = generateMockRecords(config.recordsState);
         
         final CountDownLatch startLatch = new CountDownLatch(1);
