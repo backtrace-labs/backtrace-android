64cadef3c04538e0bbccc4a6a29f146ec56cde72
Bartosz Litwiniuk <>
2025-07-10 20:41:04 +0200

    Gson throwable serializer

--- backtrace-library/src/main/java/backtraceio/library/common/serialization/ThrowableAdapter.java
+++ backtrace-library/src/main/java/backtraceio/library/common/serialization/ThrowableAdapter.java
@@ -4,6 +4,10 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
@@ -12,71 +16,88 @@ import java.util.List;
 class ThrowableAdapter extends TypeAdapter<Throwable> {
     @Override
     public void write(JsonWriter out, Throwable value) throws IOException {
-        out.beginObject();
-        out.name("message").value(value.getMessage());
-        out.name("class").value(value.getClass().getName());
-        out.name("stack-trace");
-        out.beginArray();
-        for (StackTraceElement element : value.getStackTrace()) {
-            out.value(element.toString());
+        JSONObject json = new JSONObject();
+        try {
+            json.put("message", value.getMessage());
+            json.put("class", value.getClass().getName());
+            json.put("stack-trace", parseStacktraceElements(value.getStackTrace()));
+
+        } catch (JSONException e) {
+            // TODO: improve
+            e.printStackTrace();
         }
-        out.endArray();
-        out.endObject();
+
+        out.jsonValue(json.toString());
+    }
+
+    public JSONArray parseStacktraceElements(StackTraceElement[] stackTraceElements) throws JSONException {
+        JSONArray stackTraceArray = new JSONArray();
+        for (StackTraceElement element : stackTraceElements) {
+            JSONObject elementJson = new JSONObject();
+            elementJson.put("declaring-class", element.getClassName());
+            elementJson.put("method-name", element.getMethodName());
+            elementJson.put("file-name", element.getFileName());
+            elementJson.put("line-number", element.getLineNumber());
+            stackTraceArray.put(elementJson);
+        }
+        return stackTraceArray;
     }
 
     @Override
     public Throwable read(JsonReader in) throws IOException {
-        String message = null;
-        String className = null;
-        List<String> stackLines = new ArrayList<>();
-        Throwable cause = null;
-
-        in.beginObject();
-        while (in.hasNext()) {
-            switch (in.nextName()) {
-                case "message":
-                    message = in.nextString();
-                    break;
-                case "class":
-                    className = in.nextString();
-                    break;
-                case "stackTrace":
-                    in.beginArray();
-                    while (in.hasNext()) {
-                        stackLines.add(in.nextString());
-                    }
-                    in.endArray();
-                    break;
-                case "cause":
-                    cause = read(in); // recursive call
-                    break;
-                default:
-                    in.skipValue();
-            }
-        }
-        in.endObject();
-
-        // Attempt to recreate Throwable (falling back to RuntimeException)
-        Throwable t;
-        try {
-            Class<?> clazz = Class.forName(className);
-            Constructor<?> constructor = clazz.getConstructor(String.class);
-            t = (Throwable) constructor.newInstance(message);
-        } catch (Exception e) {
-            t = new RuntimeException(message);
-        }
-
-        if (cause != null) {
-            t.initCause(cause);
-        }
-
-        // Recreate stack trace (best effort)
-        List<StackTraceElement> elements = new ArrayList<>();
-        for (String line : stackLines) {
-            elements.add(new StackTraceElement("unknown", line, null, -1));
-        }
-        t.setStackTrace(elements.toArray(new StackTraceElement[0]));
-
-        return t;
+        return this.fromJson(in.toString());
+//        in.
+//        String message = null;
+//        String className = null;
+//        List<String> stackLines = new ArrayList<>();
+//        Throwable cause = null;
+//
+//        in.beginObject();
+//        while (in.hasNext()) {
+//            switch (in.nextName()) {
+//                case "message":
+//                    message = in.nextString();
+//                    break;
+//                case "class":
+//                    className = in.nextString();
+//                    break;
+//                case "stackTrace":
+//                    in.beginArray();
+//                    while (in.hasNext()) {
+//                        stackLines.add(in.nextString());
+//                    }
+//                    in.endArray();
+//                    break;
+//                case "cause":
+//                    cause = read(in); // recursive call
+//                    break;
+//                default:
+//                    in.skipValue();
+//            }
+//        }
+//        in.endObject();
+//
+//        // Attempt to recreate Throwable (falling back to RuntimeException)
+//        Throwable t;
+//        try {
+//            Class<?> clazz = Class.forName(className);
+//            Constructor<?> constructor = clazz.getConstructor(String.class);
+//            t = (Throwable) constructor.newInstance(message);
+//        } catch (Exception e) {
+//            t = new RuntimeException(message);
+//        }
+//
+//        if (cause != null) {
+//            t.initCause(cause);
+//        }
+//
+//        // Recreate stack trace (best effort)
+//        List<StackTraceElement> elements = new ArrayList<>();
+//        for (String line : stackLines) {
+//            elements.add(new StackTraceElement("unknown", line, null, -1));
+//        }
+//        t.setStackTrace(elements.toArray(new StackTraceElement[0]));
+//
+//        return t;
     }
 }
--- backtrace-library/src/test/java/backtraceio/library/BacktraceSerializeHelperTest.java
+++ backtrace-library/src/test/java/backtraceio/library/BacktraceSerializeHelperTest.java
@@ -12,7 +12,50 @@ import backtraceio.library.models.types.BacktraceResultStatus;
 
 public class BacktraceSerializeHelperTest {
     @Test
+    public void testSerializeException() {
+        // GIVEN
+        Exception exception = new Exception("test-msg");
+        StackTraceElement[] elements = new StackTraceElement[1];
+        elements[0] = new StackTraceElement("class", "method", "file-name", 1);
+        exception.setStackTrace(elements);
+        // WHEN
+        String json = BacktraceSerializeHelper.toJson(exception);
+        String result = "{\n" +
+                "  \"detail-message\" : \"test-msg\",\n" +
+                "  \"stack-trace\" : [ {\n" +
+                "    \"declaring-class\" : \"class\",\n" +
+                "    \"method-name\" : \"method\",\n" +
+                "    \"file-name\" : \"file-name\",\n" +
+                "    \"line-number\" : 1,\n" +
+                "    \"format\" : 0\n" +
+                "  } ],\n" +
+                "  \"suppressed-exceptions\" : [ ]\n" +
+                "}";
+        // THEN
+        assertEquals("{\"message\":\"result-message\",\"status\":\"Ok\"}", json);
+    }
+
+    @Test
+    public void testSerializeAndDeserializeException() {
+        // GIVEN
+        Exception exception = new Exception("test-msg");
+        StackTraceElement[] elements = new StackTraceElement[1];
+        elements[0] = new StackTraceElement("class", "method", "file-name", 1);
+        exception.setStackTrace(elements);
+        // WHEN
+        String json = BacktraceSerializeHelper.toJson(exception);
+        Exception result = BacktraceSerializeHelper.fromJson(json, Exception.class);
+        // THEN
+        assertEquals(result.getMessage(), "");
+        assertEquals(result.getStackTrace().length, 1);
+//        assertEquals("{\"message\":\"result-message\",\"status\":\"Ok\"}", json);
+    }
+
+    @Test
     public void testSerialization() {
+        Throwable x = new Throwable("test");
+        x.
+        x.setStackTrace( );
         // GIVEN
         BacktraceResult backtraceResult = new BacktraceResult(null, "result-message", BacktraceResultStatus.Ok);
         // WHEN
