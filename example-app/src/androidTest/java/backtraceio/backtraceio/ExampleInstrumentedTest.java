4523b14630a4c28dc14f93eaa529026bbf4bfe71
Bartosz Litwiniuk <>
2025-08-11 23:17:01 +0200

    Add network config

--- example-app/src/androidTest/java/backtraceio/backtraceio/ExampleInstrumentedTest.java
+++ example-app/src/androidTest/java/backtraceio/backtraceio/ExampleInstrumentedTest.java
@@ -39,28 +39,34 @@ public class ExampleInstrumentedTest extends InstrumentedTest {
     private final int THREAD_SLEEP_TIME_MS = 2000;
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mActivityRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void enableMetricsAndBreadcrumbs() {
         BacktraceLogger.setLevel(LogLevel.DEBUG);
+        
+        // Android 15+ specific: Ensure network operations are allowed
+        if (android.os.Build.VERSION.SDK_INT >= 34) { // Android 15 is API 34
+            System.out.println("Android 15+ detected - applying network security workarounds");
+        }
+        
         onView(withId(R.id.enableBreadcrumbs)).perform(click());
     }
 
-//    @Test
-//    public void useAppContext() {
-//        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-//        assertEquals("backtraceio.backtraceio", appContext.getPackageName());
-//    }
-
-    
+    // @Test
+    // public void useAppContext() {
+    // // Context of the app under test.
+    // Context appContext =
+    // InstrumentationRegistry.getInstrumentation().getTargetContext();
+    // assertEquals("backtraceio.backtraceio", appContext.getPackageName());
+    // }
 
     @Test
     public void handledException() throws TimeoutException, CoronerResponseProcessingException, InterruptedException {
         // GIVEN
         System.out.println("HANDLED-EXCEPTION-TEST");
+        System.out.println("Android SDK: " + android.os.Build.VERSION.SDK_INT);
+        System.out.println("Device: " + android.os.Build.MODEL);
         final String[] rxId = new String[1];
         final Waiter waiter = new Waiter();
         mActivityRule.getScenario().onActivity(activity -> {
@@ -79,7 +85,7 @@ public class ExampleInstrumentedTest extends InstrumentedTest {
         // WHEN
         System.out.println("HANDLED-EXCEPTION-CLICK");
         onView(withId(R.id.handledException)).perform(click()); // UI action
-        waiter.await(10, TimeUnit.SECONDS, 1);
+        waiter.await(20, TimeUnit.SECONDS, 1); // Increased timeout for Android 16 compatibility
         Thread.sleep(THREAD_SLEEP_TIME_MS);
 
         // THEN
@@ -102,71 +108,74 @@ public class ExampleInstrumentedTest extends InstrumentedTest {
         Assert.assertEquals("java.lang.IndexOutOfBoundsException", resultClassifier);
     }
 
-//    @Test
-//    public void dumpWithoutCrash() throws CoronerResponseProcessingException, InterruptedException {
-//        // GIVEN
-//        CoronerResponse response = null;
-//        long timestampStart = this.getSecondsTimestampNowGMT();
-//
-//        // WHEN
-//        onView(withId(R.id.dumpWithoutCrash)).perform(click()); // UI action
-//        Thread.sleep(THREAD_SLEEP_TIME_MS * 10);
-//
-//        // THEN
-//        try {
-//            response = this.getCoronerClient().errorTypeTimestampFilter("Crash",
-//                    Long.toString(timestampStart),
-//                    Long.toString(this.getSecondsTimestampNowGMT()),
-//                    Arrays.asList("error.message"));
-//        } catch (Exception ex) {
-//            Assert.fail(ex.getMessage());
-//        }
-//
-//        Assert.assertNotNull(response);
-//        Assert.assertEquals(1, response.getResultsNumber());
-//        String val = response.getAttribute(0, "error.message", String.class);
-//        Assert.assertEquals("DumpWithoutCrash", val);
-//    }
-//
-//    //@Test
-//    public void unhandledException() throws CoronerResponseProcessingException, InterruptedException {
-//        // GIVEN
-//        CoronerResponse response = null;
-//        long timestampStart = this.getSecondsTimestampNowGMT();
-//
-//        // WHEN
-//        // UnhandledException crashes the app, so don't actually click the button
-//        // onView(withId(R.id.unhandledException)).perform(click()); // UI action
-//
-//        // call BacktraceExceptionHandler directly instead
-//        Thread.getDefaultUncaughtExceptionHandler().uncaughtException(Thread.currentThread(), new NullPointerException());
-//
-//        Thread.sleep(THREAD_SLEEP_TIME_MS);
-//
-//        // THEN
-//        try {
-//            response = this.getCoronerClient().errorTypeTimestampFilter("Crash",
-//                    Long.toString(timestampStart),
-//                    Long.toString(this.getSecondsTimestampNowGMT()),
-//                    Arrays.asList("error.message"));
-//        } catch (Exception ex) {
-//            Assert.fail(ex.getMessage());
-//        }
-//
-//        Assert.assertNotNull(response);
-//        Assert.assertEquals(1, response.getResultsNumber());
-//        String val = response.getAttribute(0, "error.message", String.class);
-//        Assert.assertEquals("Dump without crash", val);
-//    }
-//
-//    @Test
-//    public void anr() {
-//        onView(withId(R.id.anr)).perform(click());
-//    }
-//
-//    // Will break build, obviously.
-//    //@Test
-//    public void nativeCrash() {
-//        onView(withId(R.id.nativeCrash)).perform(click());
-//    }
+    // @Test
+    // public void dumpWithoutCrash() throws CoronerResponseProcessingException,
+    // InterruptedException {
+    // // GIVEN
+    // CoronerResponse response = null;
+    // long timestampStart = this.getSecondsTimestampNowGMT();
+    //
+    // // WHEN
+    // onView(withId(R.id.dumpWithoutCrash)).perform(click()); // UI action
+    // Thread.sleep(THREAD_SLEEP_TIME_MS * 10);
+    //
+    // // THEN
+    // try {
+    // response = this.getCoronerClient().errorTypeTimestampFilter("Crash",
+    // Long.toString(timestampStart),
+    // Long.toString(this.getSecondsTimestampNowGMT()),
+    // Arrays.asList("error.message"));
+    // } catch (Exception ex) {
+    // Assert.fail(ex.getMessage());
+    // }
+    //
+    // Assert.assertNotNull(response);
+    // Assert.assertEquals(1, response.getResultsNumber());
+    // String val = response.getAttribute(0, "error.message", String.class);
+    // Assert.assertEquals("DumpWithoutCrash", val);
+    // }
+    //
+    // //@Test
+    // public void unhandledException() throws CoronerResponseProcessingException,
+    // InterruptedException {
+    // // GIVEN
+    // CoronerResponse response = null;
+    // long timestampStart = this.getSecondsTimestampNowGMT();
+    //
+    // // WHEN
+    // // UnhandledException crashes the app, so don't actually click the button
+    // // onView(withId(R.id.unhandledException)).perform(click()); // UI action
+    //
+    // // call BacktraceExceptionHandler directly instead
+    // Thread.getDefaultUncaughtExceptionHandler().uncaughtException(Thread.currentThread(),
+    // new NullPointerException());
+    //
+    // Thread.sleep(THREAD_SLEEP_TIME_MS);
+    //
+    // // THEN
+    // try {
+    // response = this.getCoronerClient().errorTypeTimestampFilter("Crash",
+    // Long.toString(timestampStart),
+    // Long.toString(this.getSecondsTimestampNowGMT()),
+    // Arrays.asList("error.message"));
+    // } catch (Exception ex) {
+    // Assert.fail(ex.getMessage());
+    // }
+    //
+    // Assert.assertNotNull(response);
+    // Assert.assertEquals(1, response.getResultsNumber());
+    // String val = response.getAttribute(0, "error.message", String.class);
+    // Assert.assertEquals("Dump without crash", val);
+    // }
+    //
+    // @Test
+    // public void anr() {
+    // onView(withId(R.id.anr)).perform(click());
+    // }
+    //
+    // // Will break build, obviously.
+    // //@Test
+    // public void nativeCrash() {
+    // onView(withId(R.id.nativeCrash)).perform(click());
+    // }
 }
